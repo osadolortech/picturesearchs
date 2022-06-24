f0f8b895176fa53f3d0e6470ff7a7078256db879
osadolortech <brightosadolor8@gmail.com>
2022-06-24 07:01:30 +0100

    updated model files

--- pictureapi/models.py
+++ pictureapi/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+def upload_to(instance, filename):
+    return 'image/{filename}'.format(filename=filename)
+
+
+class PictureModel(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=upload_to, default='image/default.jpg')
+    uploaded_date = models.DateTimeField(auto_now_add=True)
