bf94ddd5b96672d23830ca2527f8400c5956d5a6
osadolortech <brightosadolor8@gmail.com>
2022-06-24 07:25:29 +0100

    updated views

--- pictureapi/views.py
+++ pictureapi/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from warnings import filters
+from jmespath import search
+from rest_framework import viewsets
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import PictureModel
+from .serializers import PictureSerializer
+from rest_framework import filters
 
-# Create your views here.
+
+
+class PictureView(viewsets.ModelViewSet):
+    queryset = PictureModel.objects.all()
+    serializer_class = PictureSerializer
+    parser_classes = [MultiPartParser,FormParser]
+    filter_backends = [filters.SearchFilter]
+    search_fileds = ['=title']
+
